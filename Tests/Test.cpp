1e2768770705c79c67ef94db356a8ef91dc3a01f
kseniadmitrievnadobrovolskaya <kseniadmitrievnadobrovolskaya@mail.ru>
2022-11-27 12:45:51 +0300

    add tests

--- Tests/Test.cpp
+++ Tests/Test.cpp
@@ -1,9 +1,23 @@
-#include "Test.hpp"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
 
 
+enum name
+{
+  answers,
+  results,  
+};
+
+
+std::ifstream fans("../answers.txt");
+std::ifstream fres("../results.txt");
+int read_file(name file, int elem);
 
 
 //--------------------------Test_Matrix<int>--------------------------
+
 
 TEST(Test_Matrix_int, Test1)
 {
@@ -37,5 +51,30 @@ TEST(Test_Matrix_int, Test5)
 
   EXPECT_EQ(read_file(answers, 5), read_file(results, 5));
 
+}
+
+
+int read_file(name file, int elem)
+{
+  static int elems[10];
+  static int flag = 0;
+
+  if (flag == 0)
+  {
+    flag++;
+    for (int i = 0; i < 5; i++)
+    {
+      fans >> elems[0*5 + i];
+    }
+    fans.close();
+
+    for (int i = 0; i < 5; i++)
+    {
+      fres >> elems[1*5 + i];
+    }
+    fres.close();
+  }
+
+  return elems[file*5 + elem - 1];
 }
 
