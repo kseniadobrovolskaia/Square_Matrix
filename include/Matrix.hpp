6a2967ff30c0d4c7e89641755cf6226670d72e40
kseniadmitrievnadobrovolskaya <kseniadmitrievnadobrovolskaya@mail.ru>
2023-01-15 20:24:04 +0300

    fix operator=

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -25,7 +25,7 @@ public:
 
 	Matrix(const Matrix<T> & mt): n_t(mt.n_t), matrix(mt.matrix){};
 	Matrix<T> & operator=(const Matrix<T> & mt);
-	Matrix(Matrix<T> && mt): n_t(mt.n_t), matrix(mt.matrix){ ~mt.matrix(); n_t = 0; };
+	Matrix(Matrix<T> && mt): n_t(mt.n_t), matrix(mt.matrix){ mt.n_t = 0; mt.matrix = nullptr; };
 	Matrix<T> & operator=(Matrix<T> && mt);
 	virtual ~Matrix() noexcept {};
 
@@ -91,8 +91,8 @@ Matrix<T> & Matrix<T>::operator=(Matrix<T> && mt)
 	}
 
 	matrix = mt.matrix;
-	~mt.matrix(); 
 	mt.n_t = 0;
+	mt.matrix = nullptr;
 
 	return *this;
 
